01f50ef99817e8b26fa4aba1c861e12d24cfd923
quocthang.it247 <quocthang.it247@gmail.com>
2019-12-23 20:46:46 +0700

    complete

--- src/components/customTable/CustomTable.tsx
+++ src/components/customTable/CustomTable.tsx
@@ -19,7 +19,7 @@ class CustomTable extends React.Component<IProps> {
         <div>
           <strong>{name}</strong>
         </div>
-        <span>Lv{level}</span>
+        <div className="customer__level">Lv{level}</div>
       </>
     );
   };
@@ -34,16 +34,42 @@ class CustomTable extends React.Component<IProps> {
     </tr>
   );
 
+  renderPagination = () => (
+    <div className="pagination">
+      <a href="#">First</a>
+      <a href="#">01</a>
+      <a href="#" className="pagination__active">
+        02
+      </a>
+      <a href="#">03</a>
+      <a href="#">04</a>
+      <a href="#">...</a>
+      <a href="#">12</a>
+      <a href="#">Last</a>
+    </div>
+  );
+
   render() {
     const { data = [], titles = this.titles } = this.props;
     return (
-      <Table className="customTable" responsive>
-        <thead>
-          <tr>{titles.map(item => this.renderHeader(item))}</tr>
-        </thead>
-        <tbody>{data.map(item => this.renderBody(item))}</tbody>
-        <div>You have .............</div>
-      </Table>
+      <>
+        <Table className="customTable" responsive>
+          <thead>
+            <tr>{titles.map(item => this.renderHeader(item))}</tr>
+          </thead>
+          <tbody>
+            {data.map(item => this.renderBody(item))}
+            <tr className="customTable__footer">
+              <td>
+                <div className="customTable__text">You have: 1200 order</div>
+              </td>
+              <td className="customTable__pagination" colSpan={4}>
+                {this.renderPagination()}
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </>
     );
   }
 }
